e9dd8abc1fe61a465ffff17d63af8fb8f87ffcf4
Denis Vieira <denisvieira05@gmail.com>
2018-10-09 23:50:57 -0300

    feature/#1 Add simple structure to styles and html to sign up screen
    - add markup on index html
    - added main components on stylesheets
    - add logo image on assets folder
    -
    
    Signed-off-by: Denis Vieira <denisvieira05@gmail.com>

--- src/index.js
+++ src/index.js
@@ -3,21 +3,3 @@ if (process.env.NODE_ENV !== 'production') {
   console.log('JavaScript is enabled in the browser');
 }
 
-import SignUp from './modules/signUp/SignUp';
-
-class MainApp extends HTMLElement {
-  constructor() {
-    super();
-
-    const shadow = this.attachShadow({ mode: 'open' });
-    const mainAppContainer = document.createElement('div');
-    const getHTMLView = () => `<sign-up /> `;
-
-    mainAppContainer.classList.add('main-app');
-    mainAppContainer.innerHTML = getHTMLView();
-
-    shadow.appendChild(mainAppContainer);
-  }
-}
-
-customElements.define('main-app', MainApp);
